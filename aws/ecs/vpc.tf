4cf9f07bf1b185095f24aeb6b920491a6d9a48db
aknysh <andriy.knysh@gmail.com>
2019-04-24 09:30:08 -0400

    [ecs/atlantis] Fix AZ count

--- aws/ecs/vpc.tf
+++ aws/ecs/vpc.tf
@@ -4,6 +4,12 @@ variable "limit_availability_zones" {
   default     = "true"
 }
 
+variable "availability_zones" {
+  type        = "list"
+  description = "List of Availability Zones"
+  default     = []
+}
+
 locals {
   name = "${var.region}"
 }
@@ -19,13 +25,14 @@ module "vpc" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  max_availability_zones = "${var.limit_availability_zones == "true" ? 2 : length(data.aws_availability_zones.available.names)}"
-  availability_zones     = "${slice(data.aws_availability_zones.available.names, 0, local.max_availability_zones)}"
+  availability_zones       = "${length(var.availability_zones) > 0 ? var.availability_zones : data.aws_availability_zones.available.names}"
+  max_availability_zones   = "${var.limit_availability_zones == "true" ? 2 : length(local.availability_zones)}"
+  availability_zones_final = "${slice(local.availability_zones, 0, local.max_availability_zones)}"
 }
 
 module "subnets" {
   source              = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=tags/0.8.0"
-  availability_zones  = "${local.availability_zones}"
+  availability_zones  = "${local.availability_zones_final}"
   max_subnet_count    = "${local.max_availability_zones}"
   namespace           = "${var.namespace}"
   stage               = "${var.stage}"
